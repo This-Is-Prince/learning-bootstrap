f67ff732968573b423c25cd33a32723c6481deaa
This-Is-Prince <this.is.kumar.prince@gmail.com>
2022-05-11 10:33:38 +0530

    col class chapter 4 completed

---
+++ yahoo_baba/ch_4_col_class/src/App.tsx
@@ -0,0 +1,50 @@
+const App = () => {
+  return (
+    <div className="container">
+      {/* <div className="row">
+        <div className="col">Col 1</div>
+        <div className="col">Col 2</div>
+        <div className="w-100"></div>
+        <div className="col">Col 3</div>
+        <div className="w-100"></div>
+        <div className="col">Col 4</div>
+        <div className="col">Col 5</div>
+        <div className="col">Col 6</div>
+        <div className="w-100"></div>
+        <div className="col">Col 7</div>
+        <div className="col">Col 8</div>
+      </div> */}
+      {/* <div className="row row-cols-3">
+        <div className="col">
+          Col 1 Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          Voluptates accusamus eos voluptatibus quibusdam nostrum culpa ad quo
+          dolore quas facilis?
+        </div>
+        <div className="col">
+          Col 2 Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil,
+          eius!
+        </div>
+        <div className="col">
+          Col 3 Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi
+          velit quam qui et laboriosam consequuntur tempora labore minima
+          consequatur, eaque modi dolore amet. Nesciunt exercitationem cum sunt
+          obcaecati repudiandae libero?
+        </div>
+        <div className="col">Col 4</div>
+        <div className="col">Col 5</div>
+        <div className="col">Col 6</div>
+        <div className="col">Col 7</div>
+        <div className="col">Col 8</div>
+      </div> */}
+      <div className="row">
+        <div className="col-md">Col 1</div>
+        <div className="col-md">Col 2</div>
+        <div className="col-md">Col 3</div>
+      </div>
+      <div className="row">Row 2</div>
+      <div className="row">Row 3</div>
+    </div>
+  );
+};
+
+export default App;
