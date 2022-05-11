21257cae357d987484399ac3945afb09d11bbb1b
This-Is-Prince <this.is.kumar.prince@gmail.com>
2022-05-11 11:27:59 +0530

    offset class chapter 6 completed

---
+++ yahoo_baba/ch_6_offset_class/src/App.tsx
@@ -0,0 +1,20 @@
+const App = () => {
+  return (
+    <main className="container">
+      <section className="row">
+        <article className="col-md-5 offset-lg-1 offset-md-3 pink">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod,
+          officiis?
+        </article>
+        <article className="col-md-3 offset-md-2 blue">
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolor
+          exercitationem odio error quo eveniet sunt soluta, recusandae, facilis
+          quam illum iusto rem! Dolorum tenetur quasi molestiae! Ullam explicabo
+          alias impedit.
+        </article>
+      </section>
+    </main>
+  );
+};
+
+export default App;
