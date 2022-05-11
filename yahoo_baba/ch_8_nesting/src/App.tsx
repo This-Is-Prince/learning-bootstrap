2f03c27e2b9439f14895cb3ed1a1fba6cb58fc69
This-Is-Prince <this.is.kumar.prince@gmail.com>
2022-05-11 12:51:29 +0530

    nesting completesd

---
+++ yahoo_baba/ch_8_nesting/src/App.tsx
@@ -0,0 +1,39 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <main className="container">
+      <section className="row">
+        <article className="col-md-6 pink">
+          <h2>Heading Goes here</h2>
+          <section className="row">
+            <article className="col-md-6">
+              <h3>Sub Heading</h3>
+              <p>
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Modi
+                cupiditate beatae maxime dolorem adipisci mollitia cum cumque
+                deleniti esse blanditiis. Incidunt unde minima animi veniam
+                maxime repudiandae. Accusantium dolores quia excepturi beatae,
+                nemo minus praesentium veritatis doloremque sint autem esse
+                reiciendis alias necessitatibus adipisci assumenda, nulla
+                quibusdam perferendis asperiores ipsum dolore. Perferendis,
+                dolores officia. Dolore non accusamus odio incidunt vero!
+              </p>
+            </article>
+            <article className="col-md-6">
+              <h3>Sub Heading</h3>
+              <p>
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Modi
+                cupiditate beatae maxime dolorem adipisci mollitia cum cumque
+                deleniti esse blanditiis. Incidunt unde minima animi veniam
+                maxime repudiandae. Accusantium dolores quia excepturi beatae,
+              </p>
+            </article>
+          </section>
+        </article>
+      </section>
+    </main>
+  );
+};
+
+export default App;
