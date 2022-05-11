d9f703438c0b8b89cc9e72a226a2ad25350cf1d5
This-Is-Prince <this.is.kumar.prince@gmail.com>
2022-05-11 10:17:28 +0530

    container and row classes chapter 3 completed

---
+++ yahoo_baba/ch_3_container_and_row_classes/src/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div className="container">
+      <div className="row">Row 1</div>
+      <div className="row">Row 2</div>
+      <div className="row">Row 3</div>
+    </div>
+  );
+};
+
+export default App;
