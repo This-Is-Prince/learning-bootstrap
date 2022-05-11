df12aa0b946b679e44a940a567be893f3cb6f402
This-Is-Prince <this.is.kumar.prince@gmail.com>
2022-05-11 13:26:13 +0530

    margin classes completed

---
+++ yahoo_baba/ch_9_margin/src/App.tsx
@@ -0,0 +1,28 @@
+const App = () => {
+  return (
+    <main className="container">
+      <section className="row">
+        {/* <article className="col-md-3 m-md-auto orange">
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptatibus
+          temporibus deleniti rem! Culpa expedita quaerat praesentium aliquam
+          nihil id ullam.
+        </article>
+        <article className="col-md-4 green">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae,
+          eaque.
+        </article> */}
+        <article className="col-sm-3 m-md-auto orange">
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptatibus
+          temporibus deleniti rem! Culpa expedita quaerat praesentium aliquam
+          nihil id ullam.
+        </article>
+        <article className="col-sm-4 green">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae,
+          eaque.
+        </article>
+      </section>
+    </main>
+  );
+};
+
+export default App;
